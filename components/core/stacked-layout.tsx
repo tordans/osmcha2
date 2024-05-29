147f2355994f82239db93ec3f0b0addb914283f3
Tobias <tobias@fixmycity.de>
2024-05-29 21:45:38 +0200

    Tailwind: Only check in Catalyst components that we actually use

--- components/core/stacked-layout.tsx
+++ components/core/stacked-layout.tsx
@@ -76,11 +76,7 @@ export function StackedLayout({
       </header>
 
       {/* Content */}
-      <main className="flex flex-1 flex-col pb-2 lg:px-2">
-        <div className="grow p-6 lg:rounded-lg lg:bg-white lg:p-10 lg:shadow-sm lg:ring-1 lg:ring-zinc-950/5 dark:lg:bg-zinc-900 dark:lg:ring-white/10">
-          <div className="mx-auto max-w-6xl">{children}</div>
-        </div>
-      </main>
+      <main className="flex flex-1 flex-col pb-2 lg:px-2">{children}</main>
     </div>
   )
 }
