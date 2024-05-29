9de160a0b3b4786943a573711e93e5b8ada05cca
Tobias <tobias@fixmycity.de>
2024-05-29 22:54:57 +0200

    About: Add testing text

--- app/(content)/about/page.tsx
+++ app/(content)/about/page.tsx
@@ -1,3 +1,47 @@
 export default function AboutPage() {
-  return <h1>About</h1>
+  return (
+    <>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis quibusdam labore quidem
+        qui, ea voluptatem provident aspernatur sed dolor voluptatibus ex tenetur amet libero
+        dignissimos eaque harum expedita repellat repellendus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis quibusdam labore quidem
+        qui, ea voluptatem provident aspernatur sed dolor voluptatibus ex tenetur amet libero
+        dignissimos eaque harum expedita repellat repellendus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis quibusdam labore quidem
+        qui, ea voluptatem provident aspernatur sed dolor voluptatibus ex tenetur amet libero
+        dignissimos eaque harum expedita repellat repellendus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis quibusdam labore quidem
+        qui, ea voluptatem provident aspernatur sed dolor voluptatibus ex tenetur amet libero
+        dignissimos eaque harum expedita repellat repellendus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis quibusdam labore quidem
+        qui, ea voluptatem provident aspernatur sed dolor voluptatibus ex tenetur amet libero
+        dignissimos eaque harum expedita repellat repellendus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis quibusdam labore quidem
+        qui, ea voluptatem provident aspernatur sed dolor voluptatibus ex tenetur amet libero
+        dignissimos eaque harum expedita repellat repellendus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis quibusdam labore quidem
+        qui, ea voluptatem provident aspernatur sed dolor voluptatibus ex tenetur amet libero
+        dignissimos eaque harum expedita repellat repellendus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis quibusdam labore quidem
+        qui, ea voluptatem provident aspernatur sed dolor voluptatibus ex tenetur amet libero
+        dignissimos eaque harum expedita repellat repellendus?
+      </p>
+    </>
+  )
 }
