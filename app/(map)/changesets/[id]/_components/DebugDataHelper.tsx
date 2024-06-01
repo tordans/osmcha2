826b1287e467d5bd9a4d1242ee01db3a896fe452
Tobias <tobias@fixmycity.de>
2024-06-01 22:16:55 +0200

    DebugMap: Allow hiding the UI

--- app/(map)/changesets/[id]/_components/DebugDataHelper.tsx
+++ app/(map)/changesets/[id]/_components/DebugDataHelper.tsx
@@ -1,9 +1,12 @@
+'use client'
 import { TOsmChaChangeset } from '@app/(map)/_components/Changeset/zod/OsmChaChangeset.zod'
 import { TOsmChaRealChangeset } from '@app/(map)/_components/Changeset/zod/OsmChaRealChangeset.zod'
 import { TOsmChaUser } from '@app/(map)/_components/Changeset/zod/OsmChaUser.zod'
 import { TOsmOrgChangeset } from '@app/(map)/_components/Changeset/zod/OsmOrgChangeset.zod'
 import { TOsmOrgUser } from '@app/(map)/_components/Changeset/zod/OsmOrgUser.zod'
+import { XMarkIcon } from '@heroicons/react/16/solid'
 import clsx from 'clsx'
+import { useState } from 'react'
 
 type Props = {
   osmChaChangeset?: TOsmChaChangeset
@@ -20,44 +23,64 @@ export const DebugDataHelper = ({
   osmChaUser,
   osmOrgUser,
 }: Props) => {
+  const [show, setShow] = useState(false)
+
   return (
     <div className="border-xl fixed right-1 top-1 z-10 flex max-h-[calc(100svh_-_1rem)] max-w-prose flex-col overflow-y-auto rounded border border-white/70 bg-pink-300 px-1 text-xs shadow-xl print:hidden">
-      <details>
-        <summary
-          className={clsx(osmChaChangeset ? 'cursor-pointer hover:underline' : 'text-gray-400')}
+      <div className="flex justify-end">
+        <button
+          onClick={() => setShow((prev) => !prev)}
+          className="flex size-5 items-center justify-center rounded-full bg-white/50 hover:bg-white"
         >
-          OSMCha Changeset
-        </summary>
-        <pre>{JSON.stringify(osmChaChangeset, undefined, 2)}</pre>
-      </details>
-      <details>
-        <summary
-          className={clsx(osmChaRealChangeset ? 'cursor-pointer hover:underline' : 'text-gray-400')}
-        >
-          OSMCha Real Changeset
-        </summary>
-        <pre>{JSON.stringify(osmChaRealChangeset, undefined, 2)}</pre>
-      </details>
-      <details>
-        <summary
-          className={clsx(osmOrgChangeset ? 'cursor-pointer hover:underline' : 'text-gray-400')}
-        >
-          OSMOrg Changeset
-        </summary>
-        <pre>{JSON.stringify(osmOrgChangeset, undefined, 2)}</pre>
-      </details>
-      <details>
-        <summary className={clsx(osmChaUser ? 'cursor-pointer hover:underline' : 'text-gray-400')}>
-          OSMCha User
-        </summary>
-        <pre>{JSON.stringify(osmChaUser, undefined, 2)}</pre>
-      </details>
-      <details>
-        <summary className={clsx(osmOrgUser ? 'cursor-pointer hover:underline' : 'text-gray-400')}>
-          OSMOrg User
-        </summary>
-        <pre>{JSON.stringify(osmOrgUser, undefined, 2)}</pre>
-      </details>
+          <XMarkIcon className="size-3" />
+        </button>
+      </div>
+      {!show && (
+        <>
+          <details>
+            <summary
+              className={clsx(osmChaChangeset ? 'cursor-pointer hover:underline' : 'text-gray-400')}
+            >
+              OSMCha Changeset
+            </summary>
+            <pre>{JSON.stringify(osmChaChangeset, undefined, 2)}</pre>
+          </details>
+          <details>
+            <summary
+              className={clsx(
+                osmChaRealChangeset ? 'cursor-pointer hover:underline' : 'text-gray-400',
+              )}
+            >
+              OSMCha Real Changeset
+            </summary>
+            <pre>{JSON.stringify(osmChaRealChangeset, undefined, 2)}</pre>
+          </details>
+          <details>
+            <summary
+              className={clsx(osmOrgChangeset ? 'cursor-pointer hover:underline' : 'text-gray-400')}
+            >
+              OSMOrg Changeset
+            </summary>
+            <pre>{JSON.stringify(osmOrgChangeset, undefined, 2)}</pre>
+          </details>
+          <details>
+            <summary
+              className={clsx(osmChaUser ? 'cursor-pointer hover:underline' : 'text-gray-400')}
+            >
+              OSMCha User
+            </summary>
+            <pre>{JSON.stringify(osmChaUser, undefined, 2)}</pre>
+          </details>
+          <details>
+            <summary
+              className={clsx(osmOrgUser ? 'cursor-pointer hover:underline' : 'text-gray-400')}
+            >
+              OSMOrg User
+            </summary>
+            <pre>{JSON.stringify(osmOrgUser, undefined, 2)}</pre>
+          </details>
+        </>
+      )}
     </div>
   )
 }
