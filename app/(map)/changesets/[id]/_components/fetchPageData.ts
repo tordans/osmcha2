a90b4692a75062aad09dd3bdecfd4f63a59875b5
Tobias <tobias@fixmycity.de>
2024-06-11 14:10:23 +0200

    Some more error debugging for non 200 responses

--- app/(map)/changesets/[id]/_components/fetchPageData.ts
+++ app/(map)/changesets/[id]/_components/fetchPageData.ts
@@ -11,24 +11,41 @@ export const fetchChangesetData = async (changesetId: string) => {
   const fetchOsmChaChangeset = fetch(`https://osmcha.org/api/v1/changesets/${changesetId}/`, {
     headers: { Authorization: `Token ${process.env.NEXT_PUBLIC_TEMPORARY_USER_TOKE}` },
   })
+  // console.info(
+  //   'Fetching `fetchOsmChaChangeset`',
+  //   `curl -I -H "Authorization: Token ${process.env.NEXT_PUBLIC_TEMPORARY_USER_TOKE}" https://osmcha.org/api/v1/changesets/${changesetId}/`,
+  // )
 
   const fetchOsmChaRealChangeset = fetch(
     `https://real-changesets.s3.us-west-2.amazonaws.com/${changesetId}.json`,
   )
+  // console.info(
+  //   'Fetching `fetchOsmChaRealChangeset`',
+  //   `curl -I https://real-changesets.s3.us-west-2.amazonaws.com/${changesetId}.json`,
+  // )
 
   const fetchOsmOrgChangeset = fetch(
     `https://api.openstreetmap.org/api/0.6/changeset/${changesetId}.json?include_discussion=true`,
   )
+  // console.info(
+  //   'Fetching `fetchOsmOrgChangeset`',
+  //   `curl -I https://api.openstreetmap.org/api/0.6/changeset/${changesetId}.json?include_discussion=true`,
+  // )
 
+  console.info('Fetching', [fetchOsmChaChangeset, fetchOsmChaRealChangeset, fetchOsmOrgChangeset])
   const [rawOsmChaChangesetResponse, rawOsmChaRealChangesetResponse, rawOsmOrgChangesetResponse] =
     await Promise.all([fetchOsmChaChangeset, fetchOsmChaRealChangeset, fetchOsmOrgChangeset])
 
   const osmChaChangeset = OsmChaChangeset.parse(await rawOsmChaChangesetResponse.json())
 
   // TODO: The real changesets are not relyable. They might not exist yet … or not anymore … or where never created successfully.
-  //  The current OsmCha can handle this case and fall back to show some information.
-  //  One idea was to use useFetchErrorsActions so set an error message that we show on the UI whenever this
+  //  The current OsmCha can handle this case and falls back to show some information.
+  //  One idea was to use `useFetchErrorsActions` (not used) so set an error message that we show on the UI whenever this
   //  However, we need to work around the client vs server components issue first.
+  // TODO: This neds to be reworked to handle non 200 status codes.
+  // However, the Zod parsing is very strict ATM, so its not easy to pass an empty response.
+  // Also the UI is not prepared to handle any non-"perfect" response.
+  // INFO: We check the status with rawOsmChaRealChangesetResponse.status === 200 and this API will return XML with status 404 when the changeset is missging on S3.
   const osmChaRealchangesetRaw = await rawOsmChaRealChangesetResponse.json()
   writeDebugFile({
     parser: OsmChaRealChangeset,
@@ -65,6 +82,7 @@ export const fetchUserData = async (userId: string) => {
     headers: { Authorization: `Token ${process.env.NEXT_PUBLIC_TEMPORARY_USER_TOKE}` },
   })
 
+  console.info('Fetching', [fetchOsmOrgUser, fetchOsmChaUser])
   const [rawOsmOrgUserResponse, rawOsmChaUserResponse] = await Promise.all([
     fetchOsmOrgUser,
     fetchOsmChaUser,
