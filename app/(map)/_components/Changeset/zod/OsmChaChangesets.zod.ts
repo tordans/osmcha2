973f6b94a4bc265eeda02fecd8569fae5b03def9
Tobias <tobias@fixmycity.de>
2024-06-11 11:23:43 +0200

    Fix zod types for Changeset list; make strict

--- app/(map)/_components/Changeset/zod/OsmChaChangesets.zod.ts
+++ app/(map)/_components/Changeset/zod/OsmChaChangesets.zod.ts
@@ -3,42 +3,45 @@ import { z } from 'zod'
 export type TOsmChaChangesets = z.infer<typeof OsmChaChangesets>
 
 const Coordinate = z.array(z.number())
-const Polygon = z.object({
+const Polygon = z.strictObject({
   type: z.literal('Polygon'),
   coordinates: z.array(z.array(Coordinate)),
 })
 
-const Properties = z.object({
+const Properties = z.strictObject({
+  area: z.number(),
+  check_date: z.coerce.date().nullable(),
   check_user: z.nullable(z.string()),
-  reasons: z.array(z.unknown()),
-  tags: z.array(
-    z.object({
-      id: z.number(),
-      name: z.string(),
-    }),
-  ),
-  features: z.array(z.unknown()),
-  user: z.string(),
-  uid: z.string(),
-  editor: z.string(),
+  checked: z.boolean(),
   comment: z.string(),
   comments_count: z.number(),
-  source: z.string(),
-  imagery_used: z.string(),
+  create: z.number(),
   date: z.string(),
+  delete: z.number(),
+  editor: z.string(),
+  features: z.array(z.unknown()),
+  harmful: z.boolean(),
+  imagery_used: z.string(),
+  is_suspect: z.boolean(),
+  metadata: z.record(z.string(), z.union([z.string(), z.number()])),
+  modify: z.number(),
+  reasons: z.array(z.unknown()),
   reviewed_features: z.array(z.unknown()),
-  checked: z.boolean(),
-  check_date: z.coerce.date().nullable(),
+  source: z.string(),
+  tag_changes: z.record(z.string(), z.array(z.string())),
+  tags: z.array(z.strictObject({ id: z.number(), name: z.string() })),
+  uid: z.string(),
+  user: z.string(),
 })
 
-const Feature = z.object({
+const Feature = z.strictObject({
   id: z.number(),
   type: z.literal('Feature'),
   geometry: Polygon,
   properties: Properties,
 })
 
-export const OsmChaChangesets = z.object({
+export const OsmChaChangesets = z.strictObject({
   type: z.literal('FeatureCollection'),
   count: z.coerce.number(),
   next: z.string().url().nullable(),
