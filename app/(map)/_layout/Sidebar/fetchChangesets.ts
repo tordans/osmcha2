d0f86bb01f2d9316bef390fdab6399abff4d083f
Tobias <tobias@fixmycity.de>
2024-06-11 20:41:24 +0200

    Cleanup

--- app/(map)/_layout/Sidebar/fetchChangesets.ts
+++ app/(map)/_layout/Sidebar/fetchChangesets.ts
@@ -23,15 +23,6 @@ export const fetchChangesets = async () => {
     apiUrl.searchParams.set('checked_by', filters.checked_by.map((f) => f.value).join(','))
   filters.uids && apiUrl.searchParams.set('uids', filters.uids.map((e) => e.value).join(','))
 
-  console.log('#xxxx', filters.checked_by, filters, apiUrl.searchParams)
-  // Object.entries(filters).forEach(([key, value]) => {
-  //   apiUrl.searchParams.set(key, JSON.stringify(value))
-  // })
-
-  // https://osmcha.org/api/v1/changesets/?page=1&page_size=25&checked_by=tordans&date__lte=2024-06-11%2005%3A19
-  // '/?filters={"checked_by":[{"label":"tordans","value":"tordans"}],"date__gte":[{"label":"","value":""}]}'
-  // 'https://osmcha.org/api/v1/changesets/?page=1&page_size=25&checked_by=tordans,Supaplex030&date__lte=2024-06-11 05:36'
-
   // TODO: Add some error handling for errors like a 500 due to mailformed params
   console.info('Fetching', apiUrl.toString())
   const rawResponse = await fetch(apiUrl.toString(), {
