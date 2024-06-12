bf05fc564aca834acdfcf4693c49461dad9330c4
Tobias <tobias@fixmycity.de>
2024-06-12 06:09:34 +0200

    Improve zod types for aoi

--- app/(map)/_data/OsmChaAoi.zod.ts
+++ app/(map)/_data/OsmChaAoi.zod.ts
@@ -2,22 +2,37 @@ import { z } from 'zod'
 
 export type TOsmChaAoi = z.infer<typeof OsmChaAoi>
 
+const Polygon = z.strictObject({
+  type: z.literal('Polygon'),
+  coordinates: z.array(z.array(z.tuple([z.number(), z.number()]))),
+})
+const MultiPolygon = z.strictObject({
+  type: z.literal('MultiPolygon'),
+  coordinates: z.array(z.array(z.array(z.tuple([z.number(), z.number()])))),
+})
+
 export const OsmChaAoi = z.strictObject({
   id: z.string(),
   type: z.literal('Feature'),
-  geometry: z.strictObject({
-    type: z.literal('Polygon'),
-    coordinates: z.array(z.array(z.tuple([z.number(), z.number()]))),
-  }),
+  geometry: z.discriminatedUnion('type', [Polygon, MultiPolygon]).nullable(),
   properties: z.strictObject({
     name: z.string(),
     filters: z.strictObject({
-      area_lt: z.string(),
-      geometry: z.string(),
-      date__gte: z.string(),
-      tag_changes: z.string(),
+      area_lt: z.string().optional(),
+      geometry: z.string().optional(),
+      date__gte: z.string().optional(),
+      date__lte: z.string().optional(),
+      tag_changes: z.string().optional(),
+      harmful: z.string().optional(),
+      checked: z.string().optional(),
+      reasons: z.string().optional(),
+      tags: z.string().optional(),
+      users: z.string().optional(),
+      editor: z.string().optional(),
+      comment: z.string().optional(),
+      in_bbox: z.string().optional(),
     }),
-    date: z.string(),
+    date: z.coerce.date(), // createdAt, it looks like there is no updatedAt
     changesets_url: z.string(),
   }),
 })
--- app/(map)/_data/OsmChaAois.zod.ts
+++ app/(map)/_data/OsmChaAois.zod.ts
@@ -5,8 +5,8 @@ export type TOsmChaAois = z.infer<typeof OsmChaAois>
 
 export const OsmChaAois = z.strictObject({
   count: z.number(),
-  next: z.string().url().optional(),
-  previous: z.string().url().optional(),
+  next: z.string().url().nullish(),
+  previous: z.string().url().nullish(),
   results: z.strictObject({
     type: z.literal('FeatureCollection'),
     features: z.array(OsmChaAoi),
