303756f75246fdc6106e7f8460b91170d397690c
Tobias <tobias@fixmycity.de>
2024-06-14 06:02:36 +0200

    Sidebar: Style Aoi title

--- app/(map)/_components/Sidebar/Sidebar.tsx
+++ app/(map)/_components/Sidebar/Sidebar.tsx
@@ -10,7 +10,11 @@ export const Sidebar = async () => {
   return (
     <nav className="group/nav h-full overflow-y-scroll">
       <div className="relative">
-        {osmChaAoi && <h1>{osmChaAoi.properties.name}</h1>}
+        {osmChaAoi && (
+          <h1 className="border-b-zinc-400 bg-zinc-100 px-3 py-2 font-semibold">
+            Saved Filter: {osmChaAoi.properties.name}
+          </h1>
+        )}
         <DebugDataHelperDialog data={osmChaAoi} title="AOI Object" />
       </div>
       <ul className="divide-y divide-gray-100">
